d9deace8adc4987ff7f9269477d531037b7ddc11
jimbiscuit <julienchandelle@gmail.com>
2023-10-03 15:15:03 +0200

    Add ButtonIcon component

---
+++ src/components/ButtonIcon/ButtonIcon.jsx
@@ -0,0 +1,42 @@
+import { Tooltip, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import EditIcon from "@mui/icons-material/Edit";
+import SettingsIcon from '@mui/icons-material/Settings';
+import DeleteIcon from "@mui/icons-material/Delete";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
+
+const iconMapping = {
+  "add" : AddIcon,
+  "download": FileDownloadIcon,
+  "upload": FileUploadIcon,
+  "edit": EditIcon,
+  "setting": SettingsIcon,
+  "delete": DeleteIcon,
+  "grab": MenuIcon,
+  "close": CloseIcon
+}
+
+export const ButtonIcon = (props) => {
+  console.log("🚀 ~ file: ButtonIcon.jsx:23 ~ ButtonIcon ~ props:", props)
+  const {onClick, className, name, color, title, id} = props;
+  const Icon = iconMapping[name]
+  const onClickProps = (data) => {
+    onClick(data, id)
+  }
+  return (
+    <Tooltip title={title} className={`tooltip ${className}`}>
+      <IconButton
+        className={`button`}
+        aria-label={title}
+        variant="outlined"
+        onClick={onClickProps}
+        color={color}
+      >
+        <Icon color={color} />
+      </IconButton>
+    </Tooltip>
+)
+}
