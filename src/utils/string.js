15e561e123e3e42dc6dbe36285118a525c548e43
jimbiscuit <julienchandelle@gmail.com>
2023-10-03 15:28:15 +0200

    Add generate id function

--- src/utils/string.js
+++ src/utils/string.js
@@ -10,3 +10,17 @@ export const stringToId = (string) => {
   output = output.replaceAll("ç", "c");
   return output;
 };
+
+export const generateId = (inputString, existingObjects) => {
+
+  let uniqueId = inputString;
+
+  let count = 1;
+  while (existingObjects.some(obj => obj.id === uniqueId)) {
+    uniqueId = `${inputString}_${count}`;
+    count++;
+  }
+
+  return uniqueId;
+
+}
